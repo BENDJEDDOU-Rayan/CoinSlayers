bcd1d7e46bba0881a22301bc1b34ee9ad69d7294
BENDJEDDOU-Rayan-2225011a <rayan.bendjeddou@etu.univ-amu.fr>
2025-01-05 20:39:44 +0100

    Ajout son de clique à chaque bouton + effet de fade quand on quitte le jeu

--- Assets/Scripts/Main Menu/Buttons/MainMenuButtons.cs
+++ Assets/Scripts/Main Menu/Buttons/MainMenuButtons.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuButtons : MonoBehaviour
 {
     [SerializeField] private SceneController sceneController;
+    [SerializeField] private FadeEffect fadeEffect;
 
     public void HandlePlay()
     {
@@ -15,11 +16,17 @@ public class MainMenuButtons : MonoBehaviour
     public void HandleExit()
     {
         SoundManager.PlaySound(SoundType.UI_BTN_PRESSED, 1f);
+        StartCoroutine(fadeEffect.FadeOutCoroutine(1f));
         Application.Quit();
     }
 
     public void HandleOpenSettings()
     {
+        SoundManager.PlaySound(SoundType.UI_BTN_PRESSED, 1f);
+    }
 
+    public void HandleCreditsOpen()
+    {
+        SoundManager.PlaySound(SoundType.UI_BTN_PRESSED, 1f);
     }
 }
